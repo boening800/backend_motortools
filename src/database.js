ad5e833770296415d652d06eb6424ceba4324d87
Miguel <boening800@gmail.com>
2020-04-24 15:51:22 -0500

    cambio de credenciales conexion mysql

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-    host:'64.225.52.96:3306',
+    host:'localhost',
+    port:3306,
     user:'root',
     password:'Motortools$01',
     database:'bd_motortools'
